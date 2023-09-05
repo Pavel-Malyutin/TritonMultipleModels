eb519d6d91a59c3ced1dad70bfad0ec1df96cb8d
Pavel Malyutin <pm5277772@gmail.com>
2023-09-05 18:46:06 +0300

    feat: added gunicorn config

---
+++ gunicorn_config.py
@@ -0,0 +1,11 @@
+import multiprocessing
+
+bind = ":8080"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'uvicorn.workers.UvicornH11Worker'
+worker_connections = 1000
+timeout = 300
+max_requests = 1000
+backlog = 2048
+threads = 8
+log_level = "debug"
